a899634e628e93fc85ca5b6a9b7e91a84e8fd04d
aymaneldawy <aymaneldawy@yahoo.com>
2021-10-23 06:59:02 -0700

    upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,39 @@
+$(function () {
+  var owl = $(".owl-carousel");
+  owl.owlCarousel({
+    loop: true,
+    margin: 10,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    autoplay: true,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true,
+      },
+      600: {
+        items: 3,
+        nav: false,
+      },
+      1000: {
+        items: 5,
+        nav: true,
+        loop: false,
+      },
+    },
+  });
+  // $('.play').on('click', function () {
+  //   owl.trigger('play.owl.autoplay', [1000])
+  // })
+  // $('.stop').on('click', function () {
+  //   owl.trigger('stop.owl.autoplay')
+  // })
+});
+
 window.addEventListener("DOMContentLoaded", () => {});
 window.addEventListener("scroll", () => {
   let carouselHeight = document.querySelector(".carousel");
-  console.log('Run...')
   if (window.scrollY > carouselHeight.getClientRects()[0].height)
     document.querySelector("header").classList.add("show-bg");
   else document.querySelector("header").classList.remove("show-bg");
