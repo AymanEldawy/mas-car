b2458b4cc7bb31c46bf762c03e56a235667ed782
aymaneldawy <aymaneldawy@yahoo.com>
2021-10-23 09:11:19 -0700

    complete stage one

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -1,35 +1,5 @@
 "use strict";
 
-$(function () {
-  var owl = $(".owl-carousel");
-  owl.owlCarousel({
-    loop: true,
-    margin: 10,
-    autoplayTimeout: 3000,
-    autoplayHoverPause: true,
-    autoplay: true,
-    responsiveClass: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: true
-      },
-      600: {
-        items: 3,
-        nav: true
-      },
-      1000: {
-        items: 5,
-        nav: true
-      }
-    }
-  }); // $('.play').on('click', function () {
-  //   owl.trigger('play.owl.autoplay', [1000])
-  // })
-  // $('.stop').on('click', function () {
-  //   owl.trigger('stop.owl.autoplay')
-  // })
-});
 window.addEventListener("DOMContentLoaded", function () {});
 window.addEventListener("scroll", function () {
   var carouselHeight = document.querySelector(".carousel");
