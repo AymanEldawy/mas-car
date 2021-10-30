ee5418bdf10da8770a9aa09c49fb12600f0356a4
aymaneldawy <aymaneldawy@yahoo.com>
2021-10-30 14:55:36 -0700

    update works

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-window.addEventListener("DOMContentLoaded", () => {});
+window.addEventListener("DOMContentLoaded", () => {
+  let globalLink = document.querySelector('.fixed-links > i');
+  globalLink.addEventListener('click',() => {
+    globalLink.classList.toggle('opened')
+    document.querySelector('.fixed-links .menu-links').classList.toggle('open-menu')
+  })
+
+});
 window.addEventListener("scroll", () => {
   let carouselHeight = document.querySelector(".carousel");
   if (window.scrollY > carouselHeight.getClientRects()[0].height)
     document.querySelector("header").classList.add("show-bg");
   else document.querySelector("header").classList.remove("show-bg");
+  
 });
